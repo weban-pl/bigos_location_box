beee7d9b4dae8f88b665fc96ba8ec5f8703bc525
kremenso <kremenso@o2.pl>
2013-02-05 19:06:55 +0100

    missiing files

--- lib/bigos_location_box/controllers/helpers.rb
+++ lib/bigos_location_box/controllers/helpers.rb
@@ -2,6 +2,10 @@ module BigosLocationBox
   module Controllers
     module Helpers
       extend ActiveSupport::Concern
+
+      def bigos_contact_box_widget ()
+        render :partial=>"bigos_location_box/module/widget"
+      end
     end
   end
 end
